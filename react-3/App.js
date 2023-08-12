a1040188ca38efa3935ecb9346e97374746da290
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-08-12 14:00:21 +0530

    react-course

---
+++ react-3/App.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", { id: "key" }, "Hello everyone!");
+
+const h1 = React.createElement("h1", { id: "key" }, "Heading 1 for parcel");
+
+const h2 = React.createElement("h2", { id: "key" }, "Heading 2 for parcel");
+
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  h1,
+  h2,
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
+
+
+// 2.15;
