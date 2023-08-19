1e93f3ebf92669bf2837fb6b725bfbc36b6d0126
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-08-19 09:53:58 +0530

    react-course

--- react-6/src/App.js
+++ react-6/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 
 
@@ -22,6 +23,7 @@ const AppLayout  = () =>{
 
   <Header />
   <Body />
+  <Footer />
  
   </>
   )
