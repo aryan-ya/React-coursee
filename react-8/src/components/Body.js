a2a4d30250f07bb3a7818d8358f8443ef80c0f3a
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-08-21 09:02:14 +0530

    react-course

---
+++ react-8/src/components/Body.js
@@ -0,0 +1,61 @@
+
+import { restaurantList } from "../contants";
+import RestaurantCard from "./RestaurantCard";
+import { useState , useEffect } from "react";
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return filterData;
+}
+
+// Body Component for body section: It contain all restaurant cards
+// We are mapping restaurantList array and passing JSON data to RestaurantCard component as props with unique key as index
+const Body = () => {
+  // useState: To create a state variable, searchText is local state variable
+  const [searchText, setSearchText] = useState("");
+  
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
+  console.log("render");
+  useEffect(() =>{
+    console.log("call and their webdency is changed");
+  }, []);
+
+  
+  
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search a restaurant you want..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            // filter the data
+            const data = filterData(searchText, restaurants);
+            // update the state of restaurants list
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
