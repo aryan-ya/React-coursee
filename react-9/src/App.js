dc5962e03debb32da0cc33ef1688c2bc267bfeec
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-08-24 18:29:42 +0530

    react-course

---
+++ react-9/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+
+
+
+
+
+
+// Body Component for body section: It contain all restaurant cards
+// We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
+
+function App() {
+  return (
+    <div className="App">
+
+<Header />
+<Body />
+<Footer />
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
