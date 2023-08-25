904477123841e4239acc3463dbe5e10d6c5d8638
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-08-25 11:58:35 +0530

    react-course

--- react-9/src/App.js
+++ react-9/src/App.js
@@ -3,12 +3,12 @@ import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { createBrowserRouter } from "react-router-dom";
 
 
 
 
-
-
+const appRouter = createBrowserRouter() 
 
 // Body Component for body section: It contain all restaurant cards
 // We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
---
+++ react-9/src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+     <h1>About page</h1>
+  
+     <p>
+     {" "}
+     this is namaste react live course chapter -7 
+     </p>
+    </div>
+  )
+
+}
+
+export default About;
