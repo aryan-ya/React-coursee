37466b7cb543a193240f645e9409fe00852342cc
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-09-10 08:36:48 +0530

    react-course

--- react-10/src/App.js
+++ react-10/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 import RestaurantMenu from "./components/RestaurantMenu";
 
